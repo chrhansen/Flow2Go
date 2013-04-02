e1ea02775aabf134789870b54249eaeb04bfa0cf
Christian Hansen <christian@lifebeatapp.com>
2013-04-01 23:06:47 -0300

    created plot data operation to calculate density, dot and histogram values in background

---
+++ Flow2Go/FGPlotDataOperation.h
@@ -0,0 +1,13 @@
+//
+//  FGPlotDataOperation.h
+//  Flow2Go
+//
+//  Created by Christian Hansen on 01/04/13.
+//  Copyright (c) 2013 Christian Hansen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FGPlotDataOperation : NSOperation
+
+@end
---
+++ Flow2Go/FGPlotDataOperation.m
@@ -0,0 +1,13 @@
+//
+//  FGPlotDataOperation.m
+//  Flow2Go
+//
+//  Created by Christian Hansen on 01/04/13.
+//  Copyright (c) 2013 Christian Hansen. All rights reserved.
+//
+
+#import "FGPlotDataOperation.h"
+
+@implementation FGPlotDataOperation
+
+@end

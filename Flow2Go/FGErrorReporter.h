1b0747c5b610a3d08b854c7ab184ea9fa6db7be7
Christian Hansen <christian@lifebeatapp.com>
2013-04-08 14:30:38 -0700

    added error reporter class

---
+++ Flow2Go/FGErrorReporter.h
@@ -0,0 +1,13 @@
+//
+//  FGErrorReporter.h
+//  Flow2Go
+//
+//  Created by Christian Hansen on 07/04/13.
+//  Copyright (c) 2013 Christian Hansen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FGErrorReporter : NSObject
+
+@end
---
+++ Flow2Go/FGErrorReporter.m
@@ -0,0 +1,13 @@
+//
+//  FGErrorReporter.m
+//  Flow2Go
+//
+//  Created by Christian Hansen on 07/04/13.
+//  Copyright (c) 2013 Christian Hansen. All rights reserved.
+//
+
+#import "FGErrorReporter.h"
+
+@implementation FGErrorReporter
+
+@end
